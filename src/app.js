de09acca05230fd93d23b05169812be7464adbaf
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-14 19:08:14 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -7,23 +7,31 @@ var btnsort = document.getElementById("btn-sort");
 var divdraw = document.getElementById("muestraCartas");
 var divbubble = document.getElementById("bubble");
 var palo = ["♦", "♥", "♠", "♣"];
-var valor = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var valor = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var color = "";
-var arr = {};
+var cartas = [];
+var i = 0;
+// var constructor = "";
+var palorandom = "";
+var valorrandom = "";
+var cantuser = 0;
 
 window.onload = function() {
   //write your code here
 };
 
 btndraw.addEventListener("click", draw);
+btnsort.addEventListener("click", prueba);
 
 function draw() {
+  // constructor = "";
+  cartas = [];
   let cantidadingresada = parseInt(inputcantidad.value);
   divdraw.innerHTML = "";
   divbubble.innerHTML = "";
-  let cartas = [];
+  divbubble.style.display = "none";
 
-  for (let i = 0; i < cantidadingresada; i++) {
+  for (i = 0; i < cantidadingresada; i++) {
     let constructorCarta = `<div
           class="card text-center border-white border-1 rounded-2 shadow-lg"
           style="width: 80px; height: 130px;"
@@ -49,10 +57,11 @@ function draw() {
     // ${constructorCarta}
     //     </div>`;
     divdraw.insertAdjacentHTML("beforeend", constructorCarta);
-    divbubble.insertAdjacentHTML("beforeend", constructorCarta);
 
-    var palorandom = Math.floor(Math.random() * palo.length);
-    var valorrandom = Math.floor(Math.random() * valor.length);
+    // divbubble.insertAdjacentHTML("beforeend", constructorCarta);
+
+    palorandom = Math.floor(Math.random() * palo.length);
+    valorrandom = Math.floor(Math.random() * valor.length);
 
     if (palo[palorandom] === "♦" || palo[palorandom] === "♥") {
       color = "red";
@@ -67,7 +76,82 @@ function draw() {
     let valorpalofoot = document.getElementById(`palopie${i}`);
     valorpalofoot.textContent = palo[palorandom];
     valorpalofoot.style.color = color;
-    // let carta = [palo[palorandom], valor[valorrandom], palo[palorandom]];
-    // cartas.push(carta);
+    cartas.push(palo[palorandom], valor[valorrandom], palo[palorandom]);
+    cantuser = cantidadingresada;
+    // constructor = constructorCarta;
   }
+}
+
+function prueba() {
+  divbubble.style.display = "grid";
+
+  for (let j = 0; j < cantuser; j++) {
+    let constructor = `<div
+    class="card text-center border-white border-1 rounded-2 shadow-lg"
+    style="width: 80px; height: 130px;"
+  >
+    <div
+      class="card-header text-start fs-5 bg-white border-white rounded-2 p-1"
+    >
+      <div id="palocabb${j}"></div>
+    </div>
+    <div
+      class="card-body fw-bold fs-4 d-flex text-center justify-content-center align-items-center p-1"
+    >
+      <div id="numerob${j}"></div>
+    </div>
+    <div
+      class="card-footer text-end fs-5 bg-white border-white rounded-2 p-1"
+    >
+      <div id="palopieb${j}"></div>
+    </div>
+  </div>`;
+    divbubble.insertAdjacentHTML("beforeend", constructor);
+    let index = 1;
+
+    let valorpalohead = document.getElementById(`palocabb${j}`);
+    valorpalohead.textContent = cartas[index - 1];
+    valorpalohead.style.color = color;
+    let valornumero = document.getElementById(`numerob${j}`);
+    valornumero.innerHTML = cartas[index];
+    let valorpalofoot = document.getElementById(`palopieb${j}`);
+    valorpalofoot.textContent = cartas[index + 1];
+    valorpalofoot.style.color = color;
+
+    let wall = cartas.length - 1; //iniciamos el wall o muro al final del array
+    while (wall > 3) {
+      while (index < wall) {
+        //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
+        if (cartas[index] > cartas[index + 3]) {
+          let aux = cartas[index];
+          let cab = cartas[index - 1];
+          let pie = cartas[index + 1];
+          cartas[index] = cartas[index + 3];
+          cartas[index + 3] = aux;
+          cartas[index - 1] = cartas[index + 2];
+          cartas[index + 2] = cab;
+          cartas[index + 1] = cartas[index + 4];
+          cartas[index + 4] = pie;
+          if (palo[palorandom] === "♦" || palo[palorandom] === "♥") {
+            color = "red";
+          } else {
+            color = "black";
+          }
+        }
+        index += 3;
+      }
+      wall -= 3; //disminuir la pared para optimizar
+    }
+  }
+
+  // let arrays = [];
+  // for (let i = 0; i < cartas.length; i += 3) {
+  //   arrays.push(cartas.slice(i, i + 3));
+  // }
+  // arrays.forEach((array, index) => {
+  //   console.log(`Array ${index + 1}: ${array}`);
+  // });
+  // console.log(cartas);
+
+  console.log(cartas);
 }
